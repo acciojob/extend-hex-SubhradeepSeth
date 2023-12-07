9c4b35576211fd0c7e0d582834eb0e48470ea52e
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2023-12-07 16:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const extendHex = (shortHex) => {
   // write your code here
+  let ans = "#";
+  
+  if(shortHex.charAt(0)==='#'){
+    if (shortHex.charAt(1) >= "A" && shortHex.charAt(1) <= "Z") {
+        shortHex = shortHex.toUpperCase();
+      }
+  }
+  else{
+    if (shortHex.charAt(0) >= "A" && shortHex.charAt(0) <= "Z") {
+        shortHex = shortHex.toUpperCase();
+      }
+  }
+  
+  for (let i = 0; i < shortHex.length; i++) {
+    if (shortHex.charAt(i) === "#") continue;
+    ans = ans + shortHex.charAt(i) + shortHex.charAt(i);
+  }
+  return ans;
 };
 
 // Do not change the code below.
